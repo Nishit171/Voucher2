3f6a1bc5feb006f24dc3bd1f8d3a9c9ba50479da
Nishit Desai <nishit@apeirosai.com>
2025-09-26 17:07:39 +0530

    kk

--- components/form-component.tsx
+++ components/form-component.tsx
@@ -191,7 +191,7 @@ export default function FormComponent() {
       console.log("[v0] Coupon API payload:", couponApiPayload)
 
       const couponApiResponse = await fetch(
-        "https://test-cms.apeirosai.com/cms/api/v1/issueCoupon",
+        "https://cms.apeirosai.com/cms/api/v1/issueCoupon",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
